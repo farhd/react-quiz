9bfd0233e81b3241841c3421f58202fb6899ee1a
Farhad <farhad@sent.com>
2017-09-16 23:04:40 +0200

    test actions

--- __test__/actions.test.js
+++ __test__/actions.test.js
@@ -1,13 +1,44 @@
-import { count, filterTable } from '../src/actions/index'
+import {
+	createQuestions,
+	answerQuestion,
+	submitQuestions,
+	resetQuestions
+} from '../src/actions/index';
 
-describe('>>> A C T I O N S <<<',()=>{
-	test('+++ actionCreator: count', () => {
-		const add = count(1)
-		expect(add).toEqual({ type:"COUNTER_UP", counter: 1 })
+describe('>>> A C T I O N S <<<', () => {
+	test('actionCreator: createQuestions', () => {
+		const actual = createQuestions();
+		const expected = {
+			type: 'CREATE_QUESTIONS'
+		};
+		expect(actual).toEqual(expected);
 	});
-	test('+++ actionCreator: filterTable', () => {
-		const string = filterTable("f")
-		expect(string).toEqual({ type: "FILTER", filter: "f" })
+
+	test('actionCreator: answerQuestion', () => {
+		const actual = answerQuestion(1, 0);
+		const expected = {
+			type: 'ANSWER_QUESTION',
+			qId: 1,
+			cId: 0
+		};
+		expect(actual).toEqual(expected);
+	});
+
+	test('actionCreator: submitQuestions', () => {
+		const actual = submitQuestions();
+		const expected = {
+			type: 'SUBMIT_QUESTIONS',
+			answers: null
+		};
+		expect(actual).toEqual(expected);
+	});
+
+	test('actionCreator: resetQuestions', () => {
+		const actual = resetQuestions();
+		const expected = {
+			type: 'RESET_QUESTIONS',
+		};
+		expect(actual).toEqual(expected);
 	});
 });
 

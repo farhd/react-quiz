09050578055bdbdbf9235e2be46b8dddc721e652
Farhad Abdullayev <farhad@dimedis.de>
2017-09-18 17:02:55 +0200

    add tests

--- __test__/reducers.test.js
+++ __test__/reducers.test.js
@@ -17,22 +17,133 @@ describe('>>> R E D U C E R S <<<',()=>{
 		);
 	});
 
+	test('+++ CREATE_QUESTIONS', () => {
+		const oldState = {
+			questions: [],
+			answers: {},
+			result: {
+				answered: 0,
+				correct: 0
+			},
+		};
+		const action = {
+			type: 'CREATE_QUESTIONS',
+		};
+		const newState = reducers(oldState, action);
+		const questionsCreated = newState.questions.length;
+
+		expect(questionsCreated).toBeGreaterThan(0);
+	});
+
 	test('+++ ANSWER_QUESTION', () => {
-		const oldState = {};
+		const oldState = {
+			questions: [],
+			answers: {},
+			result: {
+				answered: 0,
+				correct: 0
+			},
+		};
 		const action = {
 			type: 'ANSWER_QUESTION',
 			qId: 1,
-			cId: 0
+			cId: 0,
+			isCorrect: true
 		};
 		const newState = reducers(oldState, action);
 		expect(newState).toEqual(
 			{
 				questions: [],
 				answers: {
-					1: 0
+					1: {
+						cId: 0,
+						isCorrect: true
+					}
 				},
 				result: {
 					answered: 0,
+					correct: 0
+				},
+			}
+		);
+	});
+
+	test('+++ UPDATE_RESULT - positive', () => {
+		const oldState = {
+			questions: [],
+			answers: {
+				1: {
+					cId: 0,
+					isCorrect: true
+				}
+			},
+			result: {
+				answered: 0,
+				correct: 0
+			},
+		};
+		const action = {
+			type: 'UPDATE_RESULT',
+			answers: {
+				1: {
+					cId: 0,
+					isCorrect: true
+				}
+			}
+		};
+		const newState = reducers(oldState, action);
+		expect(newState).toEqual(
+			{
+				questions: [],
+				answers: {
+					1: {
+						cId: 0,
+						isCorrect: true
+					}
+				},
+				result: {
+					answered: 1,
+					correct: 1
+				},
+			}
+		);
+	});
+
+	test('+++ UPDATE_RESULT - negative', () => {
+		const oldState = {
+			questions: [],
+			answers: {
+				1: {
+					cId: 0,
+					isCorrect: false
+				}
+			},
+			result: {
+				answered: 0,
+				correct: 0
+			},
+		};
+		const action = {
+			type: 'UPDATE_RESULT',
+			answers: {
+				1: {
+					cId: 0,
+					isCorrect: false
+				}
+			}
+		};
+		const newState = reducers(oldState, action);
+		expect(newState).toEqual(
+			{
+				questions: [],
+				answers: {
+					1: {
+						cId: 0,
+						isCorrect: false
+					}
+				},
+				result: {
+					answered: 1,
 					correct: 0
 				},
 			}
